488fdae526877d3ff5f5debeb7dc6424ba05a622
Sattwik Sahu <sattwik21@iiserb.ac.in>
2023-06-05 13:06:39 +0530

    Added special prize routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./routes";
 import Attributions from "./routes/Attributions";
 import Description from "./routes/Description";
@@ -7,21 +7,26 @@ import Contribution from "./routes/Contribution";
 import HumanPractices from "./routes/HumanPractices";
 import Header from "./components/layout/Header";
 import Engineering from "./routes/Engineering";
+import Education from "./routes/specialPrize/Education";
+import Model from "./routes/specialPrize/Model";
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route index path="/" element={<Home />} />
-          <Route path="/attributions" element={<Attributions />} />
-          <Route path="/description" element={<Description />} />
-          <Route path="/contribution" element={<Contribution />} />
-          <Route path="/engineering" element={<Engineering />} />
-          <Route path="/human-practices" element={<HumanPractices />} />
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route index path="/" element={<Home />} />
+        {/* Standard */}
+        <Route path="/attributions" element={<Attributions />} />
+        <Route path="/description" element={<Description />} />
+        <Route path="/contribution" element={<Contribution />} />
+        <Route path="/engineering" element={<Engineering />} />
+        <Route path="/human-practices" element={<HumanPractices />} />
+
+        {/* Special Prizes */}
+        <Route path="/education" element={<Education />} />
+        <Route path="/model" element={<Model />} />
+      </Routes>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import * as React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/IISER-Bhopal">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
